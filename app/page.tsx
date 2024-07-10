de132209077fea5ffad4635328d910621bd2f3dc
Orphan <565982918@qq.com>
2024-07-10 14:16:57 +0800

    结构排版

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,148 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useRef, useState } from 'react'
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+import Footer from '@/components/Footer'
+import Comments from '@/components/Comments'
+import About from '@/components/About'
+import Image from 'next/image'
 
-export default function Home() {
+export default function page() {
+  const tabs = [
+    {
+      id: 1,
+      name: 'About',
+    },
+    {
+      id: 2,
+      name: 'Comments',
+    },
+  ]
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const tabRef = useRef<HTMLDivElement>(null)
+  const commentsNumber = useRef(0)
+  const [flag, setFlag] = useState(false)
+  const [tabId, setTabId] = useState(1)
+  const handlerScrollTop = (id: number) => {
+    if (id === 1) {
+      scrollRef.current!.scrollTop = Math.random()
+    } else {
+      scrollRef.current!.scrollTop = commentsNumber.current
+    }
+  }
+  const getTopNumber = (num: number) => {
+    console.log({ num })
+
+    commentsNumber.current = num - 110
+  }
+
+  const renderInfo = () => {
+    return (
+      <>
+        {flag && (
+          <div className="sm:w-32 flex sm:flex-col justify-center">
+            <div className="max-sm:w-56 text-[14px] h-10 rounded-md bg-[#0D99FF] flex items-center justify-center text-[#ffffff]">
+              <span className="max-sm:hidden">Open in Figma</span>
+              <div className="sm:hidden flex items-center">
+                <Image
+                  alt=""
+                  src="/images/bookmark.png"
+                  width={14}
+                  height={14}
+                  className="mx-1"
+                />
+                Save for later
+              </div>
+            </div>
+            <div className="sm:hidden mx-2 w-10 h-10 flex items-center justify-center border border-[#e6e6e6 rounded-md">
+              <Image alt="" src="/images/love.png" width={20} height={20} />
+            </div>
+            <div className="max-sm:hidden text-[11px] text-center mt-2 text-[#00000080]">
+              In-app purchases
+            </div>
+          </div>
+        )}
+      </>
+    )
+  }
+
+  useEffect(() => {
+    console.log(tabRef.current?.offsetTop)
+  }, [])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full h-full flex flex-col items-center overflow-hidden">
+      {/* 头部 */}
+      <Header />
+      <div
+        ref={scrollRef}
+        className="w-full no-scrollbar-y flex-1"
+        onScroll={(ev) => {
+          const { scrollTop } = ev.currentTarget
+          if (tabRef.current && scrollTop > tabRef.current.offsetTop - 100) {
+            setFlag(true)
+          } else {
+            setFlag(false)
+          }
+          console.log({ scrollTop })
+        }}
+      >
+        {/* 图文 */}
+        <Main />
+        {/* tab切换 */}
+        <div
+          ref={tabRef}
+          className={[
+            'w-full shadow-custom-shadow sticky top-0 bg-white',
+            flag ? 'h-20 max-sm:h-40' : ' h-8',
+          ].join(' ')}
+        >
+          <div className="max-w-[1216px] h-full mx-auto sm:px-8 max-sm:px-5 flex max-sm:flex-col items-center justify-between">
+            <div className="w-full flex-1 overflow-hidden h-full">
+              {flag && (
+                <h1 className="w-full text-lg truncate text-[#000000E5] mt-4 font-semibold">
+                  html.to.design — Import websites to Figma designs
+                  (web,html,css)
+                </h1>
+              )}
+              <div
+                className={[
+                  'sm:hidden w-full flex justify-start',
+                  flag && 'my-5',
+                ].join(' ')}
+              >
+                {renderInfo()}
+              </div>
+              <div className={['flex', flag && 'py-[4px]'].join(' ')}>
+                {tabs.map((item) => {
+                  return (
+                    <div
+                      className={[
+                        'text-sm text-[#00000080] mr-8 cursor-default h-8',
+                        tabId === item.id &&
+                          'font-semibold text-[#000000e5] border-b-2 border-[#2c2c2c]',
+                      ].join(' ')}
+                      key={item.id}
+                      onClick={() => {
+                        setTabId(item.id)
+                        handlerScrollTop(item.id)
+                      }}
+                    >
+                      {item.name}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="max-sm:hidden">{renderInfo()}</div>
+          </div>
         </div>
+        {/* 简介 */}
+        <About />
+        {/* 评论 */}
+        <Comments getTopNumber={getTopNumber} />
+        {/* 结尾 */}
+        <Footer />
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
