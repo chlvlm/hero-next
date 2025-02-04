7a8d57e218c8d41951f15193df46fa645fe49081
Orphan <565982918@qq.com>
2025-02-04 14:34:01 +0800

    temp

--- app/share/[fileName]/page.tsx
+++ app/share/[fileName]/page.tsx
@@ -3,28 +3,31 @@ import { Metadata } from 'next'
 type Props = {
   params: { fileName: string }
 }
-
+const domain = 'https://hero-next-opal.vercel.app'
 export function generateMetadata({ params }: Props): Metadata {
-  const imageUrl = `https://hero-next-opal.vercel.app/api/share/${params.fileName}`
+  const imageUrl = `${domain}/api/share/${params.fileName}`
+  const title = 'Share a Position on Twitter'
+  const description =
+    'Share your position picture and referral link on Twitter.'
   return {
-    title: 'Trade Share',
-    description: 'Check out this trading insight',
+    title,
+    description,
     openGraph: {
       images: [imageUrl],
     },
     twitter: {
       card: 'summary_large_image', // 重要，告诉 Twitter 这是一个包含大图的卡片
-      title: 'Trade Share',
-      description: 'Check out this trading insight',
+      title,
+      description,
       images: [imageUrl],
     },
   }
 }
 
 export default function SharePage({ params }: Props) {
-  const imageUrl = `https://hero-next-opal.vercel.app/api/share/${params.fileName}`
+  const imageUrl = `${domain}/api/share/${params.fileName}`
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-center h-screen">
       <img
         src={imageUrl}
         alt="Trade Share Image"
--- app/tiwtter/page.tsx
+++ app/tiwtter/page.tsx
@@ -1,28 +1,31 @@
 'use client'
 
 export default function Page() {
-  const imageUrl =
-    'https://hero-next-opal.vercel.app/api/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png'
-
+  const domain = 'https://hero-next-opal.vercel.app'
+  const imageUrl = `${domain}/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png`
   return (
     <div>
       <div
-        className="rounded-md bg-blue-500 p-2 w-20 my-20 flex text-white justify-center items-center cursor-pointer mx-auto"
+        className="rounded-md bg-blue-500 p-2 w-fit my-20 flex text-white justify-center items-center cursor-pointer mx-auto"
         onClick={() => {
           // 推文文本
           const tweetText = encodeURIComponent(
-            `Check out this amazing image! 😱`
+            `I just hopped into the @azex_io testnet! 🚀 
+    
+This might be the FIRST batch of #DeFi projects on #Berachain to publicly offer rewards and #BugBounty on its testnet! 😱
+    
+Invite link: `
           )
 
           // Twitter 分享链接，增加 `imageUrl` 作为分享内容
           const tweetUrl = `https://twitter.com/intent/tweet?text=${tweetText}&url=${encodeURIComponent(
-            'https://hero-next-opal.vercel.app/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png'
+            imageUrl
           )}`
 
           window.open(tweetUrl, '_blank')
         }}
       >
-        Share
+        Share Twitter
       </div>
     </div>
   )
