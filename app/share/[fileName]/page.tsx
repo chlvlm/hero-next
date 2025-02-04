469fbe32dd2d8f90b795a0586ac49999f55d00c9
Orphan <565982918@qq.com>
2025-02-04 11:12:12 +0800

    update img

--- app/share/[fileName]/page.tsx
+++ app/share/[fileName]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
 
 type Props = {
   params: { fileName: string }
@@ -25,7 +26,7 @@ export default function SharePage({ params }: Props) {
   const imageUrl = `https://hero-next-opal.vercel.app/api/share/${params.fileName}`
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <img
+      <Image
         src={imageUrl}
         alt="Trade Share Image"
         className="max-w-full h-auto"
