dffaae4c05259ef34f1a4698fa1f9dd238f25f5d
Orphan <565982918@qq.com>
2025-02-04 10:45:10 +0800

    temp 1

--- app/tiwtter/layout.tsx
+++ app/tiwtter/layout.tsx
@@ -1,16 +1,23 @@
-export const metadata = {
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'Next.js',
     description: 'The React Framework for the Web',
-    siteId: '1467726470533754880',
-    creator: '@nextjs',
-    creatorId: '1467726470533754880',
+    images: [
+      'https://hero-next-opal.vercel.app/api/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png',
+    ], // Must be an absolute URL
+  },
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
     images: [
       'https://hero-next-opal.vercel.app/api/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png',
     ], // Must be an absolute URL
   },
 }
+
 export default function TwitterLayout({
   children,
 }: Readonly<{
