4fa65913c73c6696b27d35e7773bd9a581a361bc
Orphan <565982918@qq.com>
2025-02-04 10:38:00 +0800

    update 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,29 +10,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Twitter Card metadata */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Join the @azex_io testnet! 🚀" />
-        <meta
-          name="twitter:description"
-          content="This might be the FIRST batch of #DeFi projects on #Berachain to publicly offer rewards and #BugBounty on its testnet!"
-        />
-        <meta
-          property="og:domain"
-          content="https://hero-next-opal.vercel.app"
-        />
-        <meta
-          name="twitter:url"
-          content={
-            'https://hero-next-opal.vercel.app/api/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png'
-          }
-        />
-        <meta
-          name="twitter:image"
-          content={
-            'https://hero-next-opal.vercel.app/api/share/BTC-AZT-true-17.46-loss-5.89-marmot88-101333.8503-104425.6167.png'
-          }
-        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
