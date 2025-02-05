f4330bb858413560ae36be7327558e54d35d428a
Orphan <565982918@qq.com>
2025-02-05 17:05:01 +0800

    temp 1

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,6 @@ import { NextResponse, URLPattern } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/incentive-testnet')) {
-    return NextResponse.redirect(new URL('/incentive-testnet', request.url))
-  }
-
-  // 方法1：使用 URL 对象的 pathname
-  if (request.nextUrl.pathname.startsWith('/share/')) {
-    // 获取最后一个斜杠后的内容作为 fileName
-    const fileName = request.nextUrl.pathname.split('/').pop()
-
-    // 或者使用正则表达式
-    // const fileName = request.nextUrl.pathname.match(/\/share\/(.+)/)?.[1];
-
-    console.log('fileName:', fileName)
-    return NextResponse.redirect(new URL(`/api/share/${fileName}`, request.url))
-  }
-
   // 方法2：使用 URLPattern（更灵活的方式）
   const pattern = new URLPattern({ pathname: '/share/:fileName' })
   const match = pattern.exec(request.url)
