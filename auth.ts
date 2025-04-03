2de4448dc0ac77f2c166f09274488f0f7d12e3d9
Orphan <565982918@qq.com>
2025-04-03 15:07:27 +0800

    feat: temp

--- auth.ts
+++ auth.ts
@@ -126,13 +126,26 @@ const authOptions: AuthOptions = {
     signIn: '/auth',
   },
   callbacks: {
-    async jwt({ token, user, account, profile, session, trigger, ...rest }) {
-      return { ...token, user, account, profile, session, trigger, ...rest }
+    async jwt({ token, user, account }) {
+      console.log('user :>> ', user)
+      console.log('token :>> ', token)
+      console.log('account :>> ', account)
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+        }
+      }
+      return token
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) {
+      console.log('session :>> ', session)
+      console.log('token :>> ', token)
       session.user.username = token.username as string
       session.user.accessToken = token.accessToken as string
-      return { ...session, ...token, ...user }
+      return session
     },
   },
   session: {
